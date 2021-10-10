8bb5da64d6c036dbab8e4785d2d4486cc25fb28c
Nikita <91836902+DrNikita@users.noreply.github.com>
2021-10-10 11:04:35 +0300

    Delete 5. Basics of OOP/DragonAndTreasure directory

--- 5. Basics of OOP/DragonAndTreasure/src/by/epam/basics_of_oop/treasures/GoldTreasures.java
+++
@@ -1,9 +0,0 @@
-package by.epam.basics_of_oop.treasures;
-
-public class GoldTreasures extends Treasure {
-	public GoldTreasures(double price, String material) {
-		super();
-		super.setPrice(price);
-		super.setMaterial(material);
-	}
-}
--- 5. Basics of OOP/DragonAndTreasure/src/by/epam/basics_of_oop/treasures/Treasure.java
+++
@@ -1,7 +0,0 @@
-package by.epam.basics_of_oop.treasures;
-
-public interface Treasure {
-	TreasureType getTreasureType();
-	double getFactor();
-	double getWeight();
-}
